4848857e7f17e25c401b1ae655e6623c12a9bd05
Magnus Andersson <magnus.andersson@mollyware.se>
2016-01-18 14:27:52 +0100

    - Added support for connecting to cassandra clusters without client auth - Regenerate and updated documentation around security and keystrokes - Expanded .gitignore - Updated README to reflect client auth changes - Now uses cassandra-driver-core 3.0.0-rc1

--- src/main/scala/akka/persistence/cassandra/CassandraPluginConfig.scala
+++ src/main/scala/akka/persistence/cassandra/CassandraPluginConfig.scala
@@ -10,6 +10,7 @@ import scala.collection.JavaConverters._
 import scala.concurrent.duration._
 import com.datastax.driver.core.JdkSSLOptions
 
+case class StorePathPasswordConfig( path: String, password: String )
 
 class CassandraPluginConfig(config: Config) {
 
@@ -56,16 +57,22 @@ class CassandraPluginConfig(config: Config) {
   }
 
   if (config.hasPath("ssl")) {
-    val trustStorePath: String = config.getString("ssl.truststore.path")
-    val trustStorePW: String = config.getString("ssl.truststore.password")
-    val keyStorePath: String = config.getString("ssl.keystore.path")
-    val keyStorePW: String = config.getString("ssl.keystore.password")
+    val trustStore = StorePathPasswordConfig( 
+        config.getString("ssl.truststore.path"), 
+        config.getString("ssl.truststore.password") 
+      )
 
-    val context = SSLSetup.constructContext(
-      trustStorePath,
-      trustStorePW,
-      keyStorePath,
-      keyStorePW)
+    val keyStore:Option[StorePathPasswordConfig] = 
+      if(config.hasPath("ssl.keystore")) {
+        val keyStore = StorePathPasswordConfig(
+          config.getString("ssl.keystore.path"),
+          config.getString("ssl.keystore.password")
+        )
+        Some(keyStore)
+      } else None
+
+
+    val context = SSLSetup.constructContext(trustStore, keyStore)
 
     // FIXME there is also a NettySSLOptions? what about SSLOptions?
     clusterBuilder.withSSL(JdkSSLOptions.builder.withSSLContext(context).build())
