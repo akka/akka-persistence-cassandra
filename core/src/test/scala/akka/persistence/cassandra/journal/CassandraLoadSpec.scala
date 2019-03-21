3864f7439a02e879331048d7858e37d536e16a33
Patrik Nordwall <patrik.nordwall@gmail.com>
2019-03-21 11:12:05 +0100

    add load test with Typed EventSourcedBehavior

--- core/src/test/scala/akka/persistence/cassandra/journal/CassandraLoadSpec.scala
+++ core/src/test/scala/akka/persistence/cassandra/journal/CassandraLoadSpec.scala
@@ -4,15 +4,15 @@
 
 package akka.persistence.cassandra.journal
 
+import scala.concurrent.duration._
+
 import akka.actor._
 import akka.persistence._
-import akka.persistence.cassandra.{ CassandraLifecycle, CassandraSpec }
+import akka.persistence.cassandra.CassandraLifecycle
+import akka.persistence.cassandra.CassandraSpec
 import akka.testkit._
 import com.typesafe.config.ConfigFactory
 import org.scalatest._
-
-import scala.concurrent.duration._
-import scala.language.postfixOps
 
 object CassandraLoadSpec {
   val config = ConfigFactory
@@ -27,7 +27,7 @@ object CassandraLoadSpec {
     })
     .withFallback(CassandraLifecycle.config)
 
-  trait Measure extends { this: Actor =>
+  trait Measure { this: Actor =>
     val NanoToSecond = 1000.0 * 1000 * 1000
 
     var startTime: Long = 0L
@@ -44,92 +44,147 @@ object CassandraLoadSpec {
     def stopMeasure(): Unit = {
       stopSequenceNr = lastSequenceNr
       stopTime = System.nanoTime
-      sender ! (NanoToSecond * (stopSequenceNr - startSequenceNr) / (stopTime - startTime))
+      sender() ! (NanoToSecond * (stopSequenceNr - startSequenceNr) / (stopTime - startTime))
     }
 
     def lastSequenceNr: Long
   }
 
-  class ProcessorA(val persistenceId: String) extends PersistentActor with Measure {
-    def receiveRecover: Receive = handle
+  object Processor {
+    def props(persistenceId: String, receiver: Option[ActorRef]): Props =
+      Props(new Processor(persistenceId, receiver))
+  }
 
-    def receiveCommand: Receive = {
-      case c @ "start" =>
-        deferAsync(c) { _ =>
-          startMeasure(); sender ! "started"
-        }
-      case c @ "stop" =>
-        deferAsync(c) { _ =>
-          stopMeasure()
-        }
-      case payload: String =>
-        persistAsync(payload)(handle)
+  class Processor(override val persistenceId: String, receiver: Option[ActorRef]) extends PersistentActor with Measure {
+    override def receiveRecover: Receive = onEvent
+
+    override def receiveCommand: Receive = {
+      case c @ "start"     => onStart(c)
+      case c @ "stop"      => onStop(c)
+      case payload: String => onCommand(payload)
     }
 
-    def handle: Receive = {
-      case _: String =>
+    def onStart(c: String): Unit = {
+      startMeasure()
+      sender() ! "started"
+    }
+
+    def onStop(c: String): Unit = {
+      stopMeasure()
+    }
+
+    def onCommand(payload: String): Unit =
+      persist(payload)(onEvent)
+
+    def onEvent: Receive = {
+      case payload: String =>
+        receiver match {
+          case None    =>
+          case Some(r) => r ! s"$payload-$lastSequenceNr"
+        }
     }
   }
 
-  class ProcessorB(val persistenceId: String, failAt: Option[Long], receiver: ActorRef) extends PersistentActor {
-    def receiveRecover: Receive = handle
-
-    def receiveCommand: Receive = {
-      case payload: String => persistAsync(payload)(handle)
-    }
-
-    def handle: Receive = {
-      case payload: String =>
-        receiver ! s"${payload}-${lastSequenceNr}"
-    }
+  object AsyncProcessor {
+    def props(persistenceId: String, receiver: Option[ActorRef]): Props =
+      Props(new AsyncProcessor(persistenceId, receiver))
   }
+
+  class AsyncProcessor(persistenceId: String, receiver: Option[ActorRef]) extends Processor(persistenceId, receiver) {
+    override def onStart(c: String): Unit = {
+      deferAsync(c) { _ =>
+        startMeasure()
+        sender() ! "started"
+      }
+    }
+
+    override def onStop(c: String): Unit = {
+      deferAsync(c) { _ =>
+        stopMeasure()
+      }
+    }
+
+    override def onCommand(payload: String): Unit =
+      persistAsync(payload)(onEvent)
+
+  }
+
 }
 
-import akka.persistence.cassandra.journal.CassandraLoadSpec._
+class CassandraLoadSpec
+    extends CassandraSpec(CassandraLoadSpec.config)
+    with ImplicitSender
+    with WordSpecLike
+    with Matchers {
 
-class CassandraLoadSpec extends CassandraSpec(config) with ImplicitSender with WordSpecLike with Matchers {
+  import CassandraLoadSpec._
 
   // use PropertyFileSnitch with cassandra-topology.properties
   override def cassandraConfigResource: String = "test-embedded-cassandra-net.yaml"
 
-  "A Cassandra journal" should {
-    "have some reasonable write throughput" in {
-      val warmCycles = 100L
-      val loadCycles = 1000L
+  private def testThroughput(processor: ActorRef): Unit = {
+    val warmCycles = 100L
+    val loadCycles = 2000L
 
-      val processor1 = system.actorOf(Props(classOf[ProcessorA], "p1a"))
-      (1L to warmCycles).foreach { i =>
-        processor1 ! "a"
-      }
-      processor1 ! "start"
-      expectMsg("started")
-      (1L to loadCycles).foreach { i =>
-        processor1 ! "a"
-      }
-      processor1 ! "stop"
-      expectMsgPF(100 seconds) {
-        case throughput: Double => println(f"\nthroughput = $throughput%.2f persistent commands per second")
+    (1L to warmCycles).foreach { i =>
+      processor ! "a"
+    }
+    processor ! "start"
+    expectMsg("started")
+    (1L to loadCycles).foreach { i =>
+      processor ! "a"
+    }
+    processor ! "stop"
+    expectMsgPF(100.seconds) {
+      case throughput: Double => println(f"throughput = $throughput%.2f persistent events per second")
+    }
+  }
+
+  private def testLoad(processor: ActorRef, startAgain: () => ActorRef): Unit = {
+    val cycles = 1000L
+
+    (1L to cycles).foreach { i =>
+      processor ! "a"
+    }
+    (1L to cycles).foreach { i =>
+      expectMsg(s"a-$i")
+    }
+
+    val processor2 = startAgain()
+    (1L to cycles).foreach { i =>
+      expectMsg(s"a-$i")
+    }
+
+    processor2 ! "b"
+    expectMsg(s"b-${cycles + 1L}")
+  }
+
+  // increase for serious testing
+  private val iterations = 3
+
+  "Untyped PersistentActor with Cassandra journal" should {
+    "have some reasonable write throughput for persist" in {
+      val processor = system.actorOf(Processor.props("p1", None))
+      (1 to iterations).foreach { _ =>
+        testThroughput(processor)
       }
     }
 
-    "work properly under load" in {
-      val cycles = 1000L
-
-      val processor1 = system.actorOf(Props(classOf[ProcessorB], "p1b", None, self))
-      (1L to cycles).foreach { i =>
-        processor1 ! "a"
+    "have some reasonable write throughput for persistAsync" in {
+      val processor = system.actorOf(AsyncProcessor.props("p1a", None))
+      (1 to iterations).foreach { _ =>
+        testThroughput(processor)
       }
-      (1L to cycles).foreach { i =>
-        expectMsg(s"a-${i}")
-      }
+    }
 
-      val processor2 = system.actorOf(Props(classOf[ProcessorB], "p1b", None, self))
-      (1L to cycles).foreach { i =>
-        expectMsg(s"a-${i}")
-      }
+    "work properly under load for persist" in {
+      val processor = system.actorOf(Processor.props("p2", Some(testActor)))
+      testLoad(processor, () => system.actorOf(Processor.props("p2", Some(testActor))))
+    }
 
-      processor2 ! "b"
-      expectMsg(s"b-${cycles + 1L}")
+    "work properly under load for persistAsync" in {
+      val processor = system.actorOf(AsyncProcessor.props("p2a", Some(testActor)))
+      testLoad(processor, () => system.actorOf(AsyncProcessor.props("p2a", Some(testActor))))
     }
   }
 }
