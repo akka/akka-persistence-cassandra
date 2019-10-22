b5b02c02bb0e02264cf2a83173b0c2abf1a0c937
Patrik Nordwall <patrik.nordwall@gmail.com>
2019-10-22 12:43:49 +0200

    Revert "BulkTagWrite from terminated actor"
    This reverts commit db6666dd9a4f1e35e4d07dc810fb3e71b50d8714.

--- core/src/test/scala/akka/persistence/cassandra/CassandraLifecycle.scala
+++ core/src/test/scala/akka/persistence/cassandra/CassandraLifecycle.scala
@@ -40,7 +40,7 @@ object CassandraLifecycle {
     akka.test.timefactor = $${?AKKA_TEST_TIMEFACTOR}
     akka.persistence.journal.plugin = "cassandra-journal"
     akka.persistence.snapshot-store.plugin = "cassandra-snapshot-store"
-    cassandra-journal.circuit-breaker.call-timeout = 1s # FIXME temporary reproducer
+    cassandra-journal.circuit-breaker.call-timeout = 30s
     akka.test.single-expect-default = 20s
     akka.test.filter-leeway = 20s
     akka.actor.serialize-messages=on
