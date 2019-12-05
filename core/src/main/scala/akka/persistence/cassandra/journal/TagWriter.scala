be286da0ef5ec50ed263fe117abc094660c41052
Christopher Batey <christopher.batey@gmail.com>
2019-12-05 13:29:22 +0000

    Remove test for disabling metrics as they are disabled by default

--- core/src/main/scala/akka/persistence/cassandra/journal/TagWriter.scala
+++ core/src/main/scala/akka/persistence/cassandra/journal/TagWriter.scala
@@ -135,7 +135,7 @@ import scala.concurrent.duration._
         sender() ! FlushComplete
       }
     case TagWrite(_, payload, _) =>
-      // FIXME, keeping this sorted is over kill. We only need to know if a new timebucket is
+      // TODO keeping this sorted is over kill. We only need to know if a new timebucket is
       // reached to force a flush or that the batch size is met
       val (newTagPidSequenceNrs, events) =
         assignTagPidSequenceNumbers(payload.toVector, tagPidSequenceNrs)
@@ -204,7 +204,7 @@ import scala.concurrent.duration._
         case Some(replyTo) =>
           log.debug("External flush request")
           if (sortedBuffer.nonEmpty) {
-            // FIXME, break into batches
+            // TODO break into batches
             write(sortedBuffer, Vector.empty[(Serialized, TagPidSequenceNr)], tagPidSequenceNrs, awaitingFlush)
           } else {
             replyTo ! FlushComplete
