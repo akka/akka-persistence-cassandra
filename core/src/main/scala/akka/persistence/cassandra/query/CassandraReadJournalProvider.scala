a3603cdb68a2afb8fecac635ce176535be9de0eb
Patrik Nordwall <patrik.nordwall@gmail.com>
2020-02-05 14:31:15 +0100

    fix minor todo in CassandraReadJournalProvider

--- core/src/main/scala/akka/persistence/cassandra/query/CassandraReadJournalProvider.scala
+++ core/src/main/scala/akka/persistence/cassandra/query/CassandraReadJournalProvider.scala
@@ -7,20 +7,12 @@ package akka.persistence.cassandra.query
 import akka.actor.ExtendedActorSystem
 import akka.persistence.query.ReadJournalProvider
 import com.typesafe.config.Config
-import scala.util.control.NonFatal
 
 class CassandraReadJournalProvider(system: ExtendedActorSystem, config: Config, configPath: String)
     extends ReadJournalProvider {
 
   override val scaladslReadJournal: scaladsl.CassandraReadJournal =
-    try {
-      new scaladsl.CassandraReadJournal(system, config, configPath)
-    } catch {
-      case NonFatal(e) =>
-        // TODO can be removed when https://github.com/akka/akka/issues/18976 is fixed
-        system.log.error(e, "Failed to initialize CassandraReadJournal")
-        throw e
-    }
+    new scaladsl.CassandraReadJournal(system, config, configPath)
 
   override val javadslReadJournal: javadsl.CassandraReadJournal =
     new javadsl.CassandraReadJournal(scaladslReadJournal)
