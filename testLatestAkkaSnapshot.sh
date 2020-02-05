501219151aef8f27d09c4eb8f42a98840b3a0971
Christopher Batey <christopher.batey@gmail.com>
2020-02-05 13:51:33 +0000

    Rely on ordering from repo

--- testLatestAkkaSnapshot.sh
+++ testLatestAkkaSnapshot.sh
@@ -1,6 +1,7 @@
-VERSION=$(curl https://repo.akka.io/snapshots/com/typesafe/akka/akka-actor_2.13/ | grep -o '2.6\.[0-9]*+[0-9]*-[0-9a-z]*' | sort -r | head -n 1)
+VERSION=$(curl https://repo.akka.io/snapshots/com/typesafe/akka/akka-actor_2.13/ | grep -o '2.6\.[0-9]*+[0-9]*-[0-9a-z]*' | tail -n 1)
 
 echo "Running with version Akka $VERSION"
 
-sbt -jvm-opts .jvmopts-travis -Doverride-akka.version=$VERSION test
+sbt -Doverride.akka.version=$VERSION test
+
 
