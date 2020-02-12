a0c3eccb23549a863e7cc63c6d26052f175a1781
Enno Runne <458526+ennru@users.noreply.github.com>
2020-02-12 10:46:59 +0100

    Another javadsl flow test

--- session/src/test/java/docs/javadsl/CassandraFlowTest.java
+++ session/src/test/java/docs/javadsl/CassandraFlowTest.java
@@ -20,6 +20,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -65,9 +66,69 @@ public class CassandraFlowTest {
 
         assertThat(written.toCompletableFuture().get(4, TimeUnit.SECONDS), is(Done.done()));
 
-        CompletionStage<List<Integer>> select = CassandraSource.create(cassandraSession, "SELECT * FROM " + table).map(row -> row.getInt("id")).runWith(Sink.seq(), helper.materializer);
+        CompletionStage<List<Integer>> select = CassandraSource
+                .create(cassandraSession, "SELECT * FROM " + table)
+                .map(row -> row.getInt("id"))
+                .runWith(Sink.seq(), helper.materializer);
         List<Integer> rows = select.toCompletableFuture().get(10, TimeUnit.SECONDS);
         assertThat(new ArrayList<>(rows), hasItems(data.toArray()));
+    }
+
+
+    @Test
+    public void typedUpdate() throws InterruptedException, ExecutionException, TimeoutException {
+        String table = helper.createTableName();
+        helper.cqlSession.execute("CREATE TABLE IF NOT EXISTS " + table + " (id int PRIMARY KEY, name text, city text);");
+
+        List<Person> persons = Arrays.asList(
+                new Person(12, "John", "London"),
+                new Person(43, "Umberto", "Roma"),
+                new Person(56, "James", "Chicago")
+        );
+
+        CompletionStage<Done> written = Source.from(persons)
+                .via(CassandraFlow.create(
+                        cassandraSession,
+                        CassandraWriteSettings.defaults(),
+                        "INSERT INTO " + table + "(id, name, city) VALUES (?, ?, ?)",
+                        (person, preparedStatement) -> preparedStatement.bind(person.id, person.name, person.city)
+                ))
+                .runWith(Sink.ignore(), helper.materializer);
+
+        assertThat(written.toCompletableFuture().get(10, TimeUnit.SECONDS), is(Done.done()));
+
+        CompletionStage<List<Person>> select = CassandraSource.create(cassandraSession, "SELECT * FROM " + table)
+                .map(row -> new Person(row.getInt("id"), row.getString("name"), row.getString("city")))
+                .runWith(Sink.seq(), helper.materializer);
+        List<Person> rows = select.toCompletableFuture().get(10, TimeUnit.SECONDS);
+        assertThat(new ArrayList<>(rows), hasItems(persons.toArray()));
+    }
+
+    public static final class Person {
+        public final int id;
+        public final String name;
+        public final String city;
+
+        public Person(int id, String name, String city) {
+            this.id = id;
+            this.name = name;
+            this.city = city;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person that = (Person) o;
+            return Objects.equals(id, that.id) &&
+                    Objects.equals(name, that.name) &&
+                    Objects.equals(city, that.city);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name, city);
+        }
     }
 
 }
