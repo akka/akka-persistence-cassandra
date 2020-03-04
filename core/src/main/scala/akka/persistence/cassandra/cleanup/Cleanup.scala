7513f6009968ac39593e18730945df47647f8827
Patrik Nordwall <patrik.nordwall@gmail.com>
2020-03-04 20:37:29 +0100

    test

--- core/src/main/scala/akka/persistence/cassandra/cleanup/Cleanup.scala
+++ core/src/main/scala/akka/persistence/cassandra/cleanup/Cleanup.scala
@@ -4,13 +4,16 @@
 
 package akka.persistence.cassandra.cleanup
 
+import scala.collection.immutable
 import scala.concurrent.Future
 import scala.concurrent.duration._
-import scala.collection.immutable
+import scala.util.Failure
+import scala.util.Success
 
 import akka.Done
 import akka.actor.ActorSystem
 import akka.annotation.ApiMayChange
+import akka.event.Logging
 import akka.pattern.ask
 import akka.persistence.Persistence
 import akka.persistence.cassandra.journal.CassandraJournal
@@ -20,15 +23,27 @@ import akka.util.Timeout
 @ApiMayChange
 final class Cleanup(system: ActorSystem) {
 
+  private val log = Logging(system, getClass)
+  private val logProgressEvery = 2
   // FIXME in 1.0 we should support configurable pluginId
   private val journal = Persistence(system).journalFor("")
-  // FIXME handle when default snapshot store isn't configured
-  private val snapshotStore = Persistence(system).snapshotStoreFor("")
+  private val snapshotStore =
+    if (system.settings.config.getString("akka.persistence.snapshot-store.plugin").nonEmpty)
+      Some(Persistence(system).snapshotStoreFor(""))
+    else None
   private implicit val askTimeout: Timeout = 10.seconds
   import system.dispatcher
 
+  def deleteAll(persistenceIds: immutable.Seq[String], neverUsePersistenceIdAgain: Boolean): Future[Done] = {
+    foreach(persistenceIds, "deleteAll", pid => deleteAll(pid, neverUsePersistenceIdAgain))
+  }
+
+  def deleteAll(persistenceId: String, neverUsePersistenceIdAgain: Boolean): Future[Done] = {
+    deleteAllEvents(persistenceId, neverUsePersistenceIdAgain).flatMap(_ => deleteAllSnapshots(persistenceId))
+  }
+
   def deleteAllEvents(persistenceIds: immutable.Seq[String], neverUsePersistenceIdAgain: Boolean): Future[Done] = {
-    Future.sequence(persistenceIds.map(pid => deleteAllEvents(pid, neverUsePersistenceIdAgain))).map(_ => Done)
+    foreach(persistenceIds, "deleteAllEvents", pid => deleteAllEvents(pid, neverUsePersistenceIdAgain))
   }
 
   def deleteAllEvents(persistenceId: String, neverUsePersistenceIdAgain: Boolean): Future[Done] = {
@@ -36,11 +51,49 @@ final class Cleanup(system: ActorSystem) {
   }
 
   def deleteAllSnapshots(persistenceIds: immutable.Seq[String]): Future[Done] = {
-    Future.sequence(persistenceIds.map(pid => deleteAllSnapshots(pid))).map(_ => Done)
+    if (snapshotStore.isDefined)
+      foreach(persistenceIds, "deleteAllSnapshots", pid => deleteAllSnapshots(pid))
+    else
+      Future.successful(Done)
   }
 
   def deleteAllSnapshots(persistenceId: String): Future[Done] = {
-    (snapshotStore ? CassandraSnapshotStore.DeleteAllsnapshots(persistenceId)).mapTo[Done]
+    snapshotStore match {
+      case Some(snapshotStoreRef) =>
+        (snapshotStoreRef ? CassandraSnapshotStore.DeleteAllsnapshots(persistenceId)).mapTo[Done]
+      case None => Future.successful(Done)
+    }
+  }
+
+  private def foreach(
+      persistenceIds: immutable.Seq[String],
+      operationName: String,
+      pidOperation: String => Future[Done]): Future[Done] = {
+    val size = persistenceIds.size
+    log.info("Cleanup started {} of [{}] persistenceId.", operationName, size)
+
+    def loop(remaining: List[String], n: Int): Future[Done] = {
+      remaining match {
+        case Nil => Future.successful(Done)
+        case pid :: tail =>
+          pidOperation(pid).flatMap { _ =>
+            if (n % logProgressEvery == 0)
+              log.info("Cleanup {} [{}] of [{}].", operationName, n, size)
+            loop(tail, n + 1)
+          }
+      }
+    }
+
+    val result = loop(persistenceIds.toList, n = 1)
+
+    result.onComplete {
+      case Success(_) =>
+        log.info("Cleanup completed {} of [{}] persistenceId.", operationName, size)
+      case Failure(e) =>
+        log.error(e, "Cleanup {} failed.")
+    }
+
+    result
   }
 
 }
