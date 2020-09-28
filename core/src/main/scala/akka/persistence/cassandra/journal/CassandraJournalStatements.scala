f23dd0464526f98d00204a9026bdef4d77dbca3a
Nathan Leyton <nathan@callhandling.co.uk>
2020-09-28 15:34:59 +0800

    change selectHighestSequenceNr to use max rather than reverse sort

--- core/src/main/scala/akka/persistence/cassandra/journal/CassandraJournalStatements.scala
+++ core/src/main/scala/akka/persistence/cassandra/journal/CassandraJournalStatements.scala
@@ -280,11 +280,9 @@ import akka.persistence.cassandra.FutureDone
 
   def selectHighestSequenceNr =
     s"""
-     SELECT sequence_nr FROM $tableName WHERE
+     SELECT max(sequence_nr) FROM $tableName WHERE
        persistence_id = ? AND
        partition_nr = ?
-       ORDER BY sequence_nr
-       DESC LIMIT 1
    """
 
   def selectDeletedTo =
