d83563c89c59b1a53f3c3253d6acd5954a2127aa
Nicolas Vollmar <nvo@scaling.ch>
2021-03-10 13:33:03 +0100

    Switches to replace #864

--- core/src/test/scala/akka/persistence/cassandra/CassandraSpec.scala
+++ core/src/test/scala/akka/persistence/cassandra/CassandraSpec.scala
@@ -159,7 +159,7 @@ abstract class CassandraSpec(
         override def write(b: Int): Unit = oldOut.write(b)
       }) {
         override def println(x: Any): Unit =
-          oldOut.println(prefix + String.valueOf(x).replaceAllLiterally("\n", s"\n$prefix"))
+          oldOut.println(prefix + String.valueOf(x).replace("\n", s"\n$prefix"))
       }
 
     Console.withOut(prefixingOut) {
