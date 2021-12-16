53a4d5d038bbf9fe1f5a5b4c531c0f13bf47aa1f
Patrik Nordwall <patrik.nordwall@gmail.com>
2021-12-16 09:14:22 +0100

    Update snapshot repo for CI testing with latest Akka snapshot

--- testLatestAkkaSnapshot.sh
+++ testLatestAkkaSnapshot.sh
@@ -1,4 +1,4 @@
-VERSION=$(curl https://repo.akka.io/snapshots/com/typesafe/akka/akka-actor_2.13/ | grep -o '2.6\.[0-9]*+[0-9]*-[0-9a-z]*' | sort -V | tail -n 1)
+VERSION=$(curl https://oss.sonatype.org/content/repositories/snapshots/com/typesafe/akka/akka-actor_2.13/ | grep -o '2.6\.[0-9]*+[0-9]*-[0-9a-z]*' | sort -V | tail -n 1)
 
 echo "Running with version Akka $VERSION"
 
