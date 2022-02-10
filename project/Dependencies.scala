f50f9dd908e1c8ea67aedc957fa54d7ab264c7eb
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-02-10 08:32:31 +0100

    scalatest 3.2.11 (was 3.1.4) (#952)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -41,7 +41,7 @@ object Dependencies {
       "com.typesafe.akka" %% "akka-cluster-tools" % AkkaVersion,
       "org.scala-lang.modules" %% "scala-collection-compat" % "2.4.4",
       Logback % Test,
-      "org.scalatest" %% "scalatest" % "3.1.4" % Test,
+      "org.scalatest" %% "scalatest" % "3.2.11" % Test,
       "org.pegdown" % "pegdown" % "1.6.0" % Test,
       "org.osgi" % "org.osgi.core" % "5.0.0" % Provided) ++ akkaTestDeps.map(_ % AkkaVersion % Test)
 
