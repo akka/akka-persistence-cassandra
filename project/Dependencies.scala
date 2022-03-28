962959a54e5dfaf302db2050fb400b1446a35ad4
Scala Steward <me@scala-steward.org>
2022-03-28 22:02:23 +0200

    logback-classic 1.2.11 (was 1.2.10)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   // for example
   val AkkaManagementVersion = "1.0.6"
 
-  val Logback = "ch.qos.logback" % "logback-classic" % "1.2.10"
+  val Logback = "ch.qos.logback" % "logback-classic" % "1.2.11"
 
   val reconcilerDependencies = Seq(
     "com.typesafe.akka" %% "akka-actor-testkit-typed" % AkkaVersion % Test,
