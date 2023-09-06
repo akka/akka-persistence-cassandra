e71e430361ec0f19b0e07dbeeebcb0bf75aef80a
Scala Steward <scala_steward@virtuslab.com>
2023-09-06 13:19:17 +0000

    bump: logback-classic 1.4.11 (was 1.2.12)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   // for example
   val AkkaManagementVersion = "1.2.0"
 
-  val Logback = "ch.qos.logback" % "logback-classic" % "1.2.12"
+  val Logback = "ch.qos.logback" % "logback-classic" % "1.4.11"
 
   val reconcilerDependencies = Seq(
     "com.typesafe.akka" %% "akka-actor-testkit-typed" % AkkaVersion % Test,
