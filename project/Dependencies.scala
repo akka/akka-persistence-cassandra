3eba6c5f4ed1015688d907be7cf6eb08f8e010a6
Johan Andrén <johan@markatta.com>
2023-09-15 15:29:26 +0200

    3.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import Keys._
 object Dependencies {
   val Scala212 = "2.12.17"
   val Scala213 = "2.13.10"
-  val Scala3 = "3.3.0"
+  val Scala3 = "3.3.1"
   val Scala2Versions = Seq(Scala213, Scala212)
   val ScalaVersions = Dependencies.Scala2Versions :+ Dependencies.Scala3
 
