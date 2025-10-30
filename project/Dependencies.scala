6684c201600acae5070fed74fc8604b5b98cbe63
Johan Andrén <johan@markatta.com>
2025-10-30 15:54:39 +0100

    bump: Akka 2.10.11, Alpakka 10.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,17 +11,17 @@ object Dependencies {
   val Scala2Versions = Seq(Scala213)
   val ScalaVersions = Dependencies.Scala2Versions :+ Dependencies.Scala3
 
-  val AkkaVersion = System.getProperty("override.akka.version", "2.10.5")
+  val AkkaVersion = System.getProperty("override.akka.version", "2.10.11")
   val AkkaVersionInDocs = VersionNumber(AkkaVersion).numbers match { case Seq(major, minor, _*) => s"$major.$minor" }
   val CassandraVersionInDocs = "4.0"
   // Should be sync with the version of the driver in Alpakka Cassandra
   val CassandraDriverVersion = "4.17.0"
   val DriverVersionInDocs = "4.14"
 
-  val AlpakkaVersion = "9.0.2"
-  val AlpakkaVersionInDocs = "9.0"
+  val AlpakkaVersion = "10.0.0"
+  val AlpakkaVersionInDocs = "10.0"
   // for example
-  val AkkaManagementVersion = "1.6.2"
+  val AkkaManagementVersion = "1.6.4"
 
   val Logback = "ch.qos.logback" % "logback-classic" % "1.5.18"
 
